4944b713614652e92b7b2a692a7bdca85a10e8cb
kregoric <75627074+isicisawsum@users.noreply.github.com>
2024-02-09 18:13:24 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
+import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner in = new Scanner(System.in); //making user able to input
+        System.out.println("Enter the political party that you affiliate with. (Type ‘D’ for Democrat, R for Republican, or I for Independent):"); //prompting user to type in political party
+        String party = in.nextLine(); //defining variables
+        if (party.equals("D"))
+        {
+            System.out.println("You get a Democratic Donkey!"); //user gets Democratic Donkey after typing ‘D’
+        }
+        else if (party.equals("R"))
+        {
+            System.out.println("You get a Republican Elephant!"); //user get Republican Elephant after typing ‘R’
+        }
+        else if (party.equals("I"))
+        {
+            System.out.println("You get an Independent Person!"); //user gets Independent Person after typing I
+        }
+        else
+        {
+            System.out.println("You affiliate with another political party: " + party); //user affiliates with other political party
         }
     }
 }
